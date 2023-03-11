e76a3744851be6760442b017bb5b0bbbad75b3b9
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2023-03-10 16:07:49 -0800

    calculate winner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Header from "./components/Header";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Square from "./components/Square";
 
 function App() {
-    const [board, setBoard] = useState(Array(9).fill(null))
-    // const [count, setCount] = useState(1)
-    const [gamerX, setGamerX] = useState(true)
-    const siteName = "Tic Tac Toe Game"
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [winner, setWinner] = useState('')
+    // const [count, setCount] = useState(1);
+    const [gamerX, setGamerX] = useState(true);
+    const siteName = "Tic Tac Toe Game";
 
-
+    useEffect(() => {
+        setWinner(calculateWinner())
+    })
 
     // const onMoveHandle = (ind) => {
     //     setCount(count + 1)
@@ -23,10 +26,31 @@ function App() {
     // }
 
     const onMoveHandle = (ind) => {
-        const gamer = gamerX? 'X' : '0';
+        const gamer = gamerX ? 'X' : '0';
         setBoard(board.map((el, index) => ind === index ? gamer : el))
         setGamerX(!gamerX)
-    }
+    };
+
+    const calculateWinner = () => {
+        const winningCombination = [
+            [0, 4, 8],
+            [2, 4, 6],
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ];
+
+        for (let i = 0; i < winningCombination.length; i++) {
+            const [a, b, c] = winningCombination[i];
+            if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+                return`${board[a]} won!`
+            }
+
+        }
+    };
 
     return (
         <div className="App">
@@ -42,6 +66,8 @@ function App() {
                     />
                 ))}
             </div>
+
+            <h2>{winner}</h2>
 
         </div>
     );
